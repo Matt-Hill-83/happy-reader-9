53db5428640dd377701451e181aed5f4f342893a
Matt Hill <matt.hill.email@gmail.com>
2020-07-21 00:47:12 -0700

    asfd

--- src/components/JsonEditor2/JsonEditor2.js
+++ src/components/JsonEditor2/JsonEditor2.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react"
-import JSONEditor from "jsoneditor"
-
+import JSONEditor, { cssText } from "jsoneditor"
+import cx from "classnames"
 import "jsoneditor/dist/jsoneditor.css"
-import "./JsonEditor2.module.css"
+// import "./JsonEditor2.module.css"
+import css from "./JsonEditor2.module.scss"
 
 export default function JsonEditor2({ props }) {
   let container
@@ -11,14 +12,14 @@ export default function JsonEditor2({ props }) {
   console.log("props", props) // zzz
   console.log("jsoneditor", jsoneditor) // zzz
 
-  const options = {
-    mode: "tree",
-    onChangeJSON: props && props.onChangeJSON,
-  }
   // const { json } = props
   // const [json, setJson] = React.useState([])
 
   useEffect(() => {
+    const options = {
+      mode: "tree",
+      onChangeJSON: props && props.onChangeJSON,
+    }
     console.log("useEffect------------mounting-------------------->>>") // zzz
     // on mount
     console.log("options", options) // zzz
@@ -40,7 +41,8 @@ export default function JsonEditor2({ props }) {
 
   return (
     <div
-      className="jsoneditor-react-container"
+      // className="jsoneditor-react-container"
+      className={cx("jsoneditor-react-container", css.main)}
       ref={(elem) => (container = elem)}
     />
   )
