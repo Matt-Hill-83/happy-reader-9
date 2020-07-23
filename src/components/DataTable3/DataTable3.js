b214d5944b8796348d6a3b4197f7fa7d4a093d7d
Matt Hill <matt.hill.email@gmail.com>
2020-07-23 01:40:59 -0700

    asdf

--- src/components/DataTable3/DataTable3.js
+++ src/components/DataTable3/DataTable3.js
@@ -3,29 +3,15 @@ import { toJS } from "mobx"
 import cx from "classnames"
 import MUIDataTable from "mui-datatables"
 
-import css from "./DataTable3.module.scss"
 import DataTable3Config from "./DataTable3Config"
 
+import css from "./DataTable3.module.scss"
+
 export default function DataTable3({ props }) {
-  const [data, setData] = React.useState([])
-  const [columns, setColumns] = React.useState([])
+  const { data, columns, options } = props
+
   const defaultOptions = DataTable3Config.options || {}
-  // const [options, setOptions] = React.useState(DataTable3Config.options || {})
-
-  useEffect(() => {
-    setData(props.data)
-  }, [props.data])
-
-  useEffect(() => {
-    setColumns(props.columns)
-  }, [props.columns])
-
-  // useEffect(() => {
-  //   setOptions(props.options)
-  // }, [props.options])
-
-  const tableOptions = { ...defaultOptions, ...props.options }
-  // const tableOptions = { ...options, ...props.options }
+  const tableOptions = { ...defaultOptions, ...options }
   const className = cx(css.main, props.className || "")
 
   return (
