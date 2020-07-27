d4983593fd3fc14bbbe549615e6bdff45ad84794
Matt Hill <matt.hill.email@gmail.com>
2020-07-26 22:52:39 -0700

    asdf

--- src/Utils/Constants/Constants.js
+++ src/Utils/Constants/Constants.js
@@ -8,5 +8,17 @@ export default class Constants {
       UNHIDE: "unhide",
       UPDATE_FRAME_SET: "updateFrameSet",
     },
+    baseConditions: {
+      COMPLETED_MISSION: "completedMission",
+      CURRENT_MISSION: "currentMission",
+      COMPLETED_SCENE: "completedScene",
+      POCKET_CONTAINS: "pocketContains",
+    },
   }
+}
+
+Constants.triggers.specialConditions = {
+  [Constants.triggers.triggerTypes.UPDATE_FRAME_SET]: {
+    FRAME_SET_INDEX: "frameSetIndex",
+  },
 }
--- src/components/SubQuestConfigTool/cities.js
+++ src/components/SubQuestConfigTool/cities.js
@@ -1,26 +1,11 @@
 import React from "react"
-// import PropTypes from "prop-types"
 import MenuItem from "@material-ui/core/MenuItem"
 import FormControl from "@material-ui/core/FormControl"
 import Select from "@material-ui/core/Select"
 
 class Cities extends React.Component {
-  //   static propTypes = {
-  //     value: PropTypes.string.isRequired,
-  //     index: PropTypes.number.isRequired,
-  //     change: PropTypes.func.isRequired,
-  //   }
-
   render() {
     const { value, index, change, items } = this.props
-    // const items = [
-    //   "Aberdeen",
-    //   "Abilene",
-    //   "Akron",
-    //   "Albany",
-    //   "Albuquerque",
-    //   "Alexandria",
-    // ]
 
     return (
       <FormControl>
