f34701e39839f33e49b4b7c1af5d6463a847ce4e
Matt Hill <matt.hill.email@gmail.com>
2020-07-29 00:50:12 -0700

    asdf

--- src/components/SubQuestWizard/SubQuestWizard.js
+++ src/components/SubQuestWizard/SubQuestWizard.js
@@ -7,6 +7,7 @@ import React, { useEffect, useState } from "react"
 
 import { getSubQuestTableConfigFunc } from "./SubQuestTableConfig"
 import DataTable3 from "../DataTable3/DataTable3"
+import MyAccordian from "../MyAccordian/MyAccordian"
 import SimpleSelectObj from "../SimpleSelectObj/SimpleSelectObj"
 
 import Utils from "../../Utils/Utils"
@@ -33,6 +34,38 @@ export default function SubQuestWizard({ props }) {
     setQuestConfig(props.questConfig || {})
   }, [props.questConfig])
 
+  const renderMissions = ({ missions }) => {
+    const realScenes = props.scenes
+
+    return missions.map((mission) => {
+      const onChangeScene = (newItem) => {
+        // const { location, id } = newItem
+        // mission.name = location.name
+        // mission.id = id
+      }
+
+      // const defaultValue = missions[0]
+      const defaultValue = realScenes.find(
+        (thing) => thing.location.name === _get(mission, "item.name") || ""
+      )
+      return (
+        <div className={cx(css.sceneName, css.listItem, css.missionsList)}>
+          <span className={cx(css.listItemName)}>{mission.name}</span>
+          <span className={cx(css.listItemName)}>{"give the"}</span>
+
+          <SimpleSelectObj
+            className={css.sceneDropdown}
+            items={realScenes}
+            value={defaultValue}
+            getOptionLabel={(option) => option.location.name}
+            onChange={onChangeScene}
+          />
+          <span className={cx(css.listItemName)}>{"to the"}</span>
+        </div>
+      )
+    })
+  }
+
   const renderScenes = ({ scenes }) => {
     const realScenes = props.scenes
 
@@ -43,8 +76,6 @@ export default function SubQuestWizard({ props }) {
         scene.id = id
       }
       const defaultValue = realScenes.find((item) => item.id === scene.id)
-      console.log("realScenes", toJS(realScenes)) // zzz
-      // console.log("realScenes", toJS(realScenes.wre.wer)) // zzz
       return (
         <div className={cx(css.sceneName, css.listItem)}>
           {/* <span className={cx(css.listItemName)}>{scene.name}</span> */}
@@ -136,26 +167,37 @@ export default function SubQuestWizard({ props }) {
   const renderedItems =
     questConfig.subQuests &&
     questConfig.subQuests.map((subQuest) => {
-      // questConfig.subQuests.slice(0, 1).map((subQuest) => {
       const { triggers, scenes, missions = [] } = subQuest
 
-      return (
-        <div className={cx(css.subQuest, css.listGroup)}>
-          {subQuest.name}
-          <div className={cx(css.triggers, css.listGroup)}>
-            <span className={cx(css.listGroupTitle)}>Missions</span>
-            {renderScenes({ scenes: missions })}
-          </div>
-          <div className={cx(css.triggers, css.listGroup)}>
-            <span className={cx(css.listGroupTitle)}>Triggers</span>
-            {renderTriggers({ triggers })}
-          </div>
-          <div className={cx(css.scenes, css.listGroup)}>
-            <span className={cx(css.listGroupTitle)}>Scenes</span>
-            {renderScenes({ scenes })}
-          </div>
-        </div>
-      )
+      const items = [
+        {
+          title: <span className={cx(css.listGroupTitle)}>Missions</span>,
+          content: (
+            <div className={cx(css.triggers, css.listGroup)}>
+              {renderMissions({ missions })}
+            </div>
+          ),
+        },
+        {
+          title: <span className={cx(css.listGroupTitle)}>Triggers</span>,
+          content: (
+            <div className={cx(css.triggers, css.listGroup)}>
+              {renderTriggers({ triggers })}
+            </div>
+          ),
+        },
+        {
+          title: <span className={cx(css.listGroupTitle)}>Scenes</span>,
+          content: (
+            <div className={cx(css.scenes, css.listGroup)}>
+              {renderScenes({ scenes })}
+            </div>
+          ),
+        },
+      ]
+      const accordianProps = { items }
+
+      return <MyAccordian props={accordianProps} />
     })
   return (
     <div className={cx(css.main)}>
